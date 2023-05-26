aab36e1c92f0b49f849bd96683715c0e623c81db
BenDonovan2002 <64138097+BenDonovan2002@users.noreply.github.com>
2023-05-26 12:30:52 +0100

    Updated styling

--- LSTM/run_model.py
+++ LSTM/run_model.py
@@ -9,18 +9,26 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, LSTM
 from tensorflow.keras.optimizers.legacy import RMSprop
 
+# Constants defining the model name, the length of the text to generate,
+# the "temperature" to control randomness, and the number of emails to generate.
 MODEL_NAME = 'MODEL_NAME'
 TEXT_LENGTH = 300
 TEMPERATURE = 0.3
 EMAIL_COUNT = 100
 
 try:
-    # Validate constants
+    # Validate the defined constants. Ensures that the model name is a string,
+    # the text length and email count are integers, and the temperature is a number (int or float).
     assert isinstance(MODEL_NAME, str), "Model name must be a string."
     assert isinstance(TEXT_LENGTH, int), "Text length must be an integer."
     assert isinstance(TEMPERATURE, (int, float)), "Temperature must be a number."
     assert isinstance(EMAIL_COUNT, int), "Email count must be an integer."
 
+    # Generate a prediction from the model output.
+    # Temperature allows control over the randomness of predictions.
+    # The higher the temperature, the more random the predictions; the
+    # lower the temperature, the more deterministic the predictions.
+    # (Chollet, 2017, p. 276)
     def generate_prediction(predictions, temperature=1.0):
         predictions = np.asarray(predictions).astype('float64')
         predictions = np.log(predictions) / temperature
@@ -29,22 +37,56 @@ try:
         probabilities = np.random.multinomial(1, predictions, 1)
         return np.argmax(probabilities)
 
+    # Function to generate a text of a given length with a specified
+    # "temperature" to control randomness. It starts from a randomly
+    # chosen point in the corpus, generates characters one by
+    # one, and then appends them to the generated text.
+    def generate_text(txt_len, temp):
+        # Start the index at a random point in the given corpus.
+        # The range of the random index is limited to (len(corpus) - LEN_SEQ - 1)
+        # to ensure that the sequence length does not exceed the remaining characters in the corpus.
+        start_ind = random.randint(0, len(corpus) - LEN_SEQ - 1)
 
-    def generate_text(length, temp):
-        start_ind = random.randint(0, len(corpus) - SEQUENCE_LENGTH - 1)
-        generated = ''
-        corpus_trim = sentence = corpus[start_ind: start_ind + (SEQUENCE_LENGTH-len(generated))]
-        generated = sentence + generated
-        for i in range(length):
-            x_pred = np.zeros((1, SEQUENCE_LENGTH, len(chars)))
-            for t, char in enumerate(sentence):
-                x_pred[0, t, chars_to_indices[char]] = 1.
-            predictions = model.predict(x_pred, verbose=0)[0]
-            ind_next = generate_prediction(predictions, temp)
-            char_next = indices_to_chars[ind_next]
-            generated += char_next
-            sentence = sentence[1:] + char_next
-        return generated[len(corpus_trim):]
+        generated_text = ''
+
+        # Select a sequence of characters of length (LEN_SEQ - len(generated_text))
+        # starting from the randomly selected index. The selected characters from the
+        # corpus are assigned to 'corpus_trim' and 'output_text'.
+        corpus_trim = output_text = corpus[start_ind: start_ind + (LEN_SEQ-len(generated_text))]
+
+        generated_text = output_text + generated_text
+
+        for _ in range(txt_len):
+
+            # Initialize a tensor of shape ( 1, LEN_SEQ, len( text_chars ) ) with zeros.
+            # This tensor will be used as input to the model for generating predictions.
+            model_prediction_x = np.zeros((1, LEN_SEQ, len(text_chars)))
+            model_prediction_x = np.zeros( (1, LEN_SEQ, len( text_chars ) ) )
+
+            # Encode each character in 'output_text' to its corresponding index and
+            # set the corresponding position in the tensor to 1.
+            for model_prediction_y, pred_char in enumerate(output_text):
+                model_prediction_x[0, model_prediction_y, chars_to_indices[pred_char]] = 1
+
+
+            # Use the model to generate predictions on the input tensor.
+            predictions = model.predict(model_prediction_x, verbose=0)
+
+            # Use the 'generate_prediction' function to select the index of the next
+            # character based on the predictions.
+            text_index_next = generate_prediction(predictions[0], temp)
+
+            # Map the index of the next character back to the actual character.
+            text_character_next = indices_to_chars[text_index_next]
+
+            # Append the next character to the 'generated_text'.
+            generated_text += text_character_next
+
+            # Slide the window of 'output_text' one character to the right by
+            # removing the first character and appending the next character.
+            output_text = output_text[1:] + text_character_next
+
+        return generated_text[len(corpus_trim):]
 
 
     model = tf.keras.models.load_model('models/' + MODEL_NAME + '.model')
--- LSTM/train_model.py
+++ LSTM/train_model.py
@@ -8,59 +8,72 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, LSTM
 from tensorflow.keras.optimizers.legacy import RMSprop
 
+# Constants defining the model name, number of epochs and learning rate.
 MODEL_NAME = 'MODEL_NAME'
 EPOCHS = 32
 LEARNING_RATE = 0.01
 
 try:
-    # Validate constants
+    # Validate the defined constants. Ensures that the model name is a string,
+    # the number of epochs is an integer, and the learning rate is a number (int or float).
     assert isinstance(MODEL_NAME, str), "Model name must be a string."
     assert isinstance(EPOCHS, int), "Epochs must be an integer."
     assert isinstance(LEARNING_RATE, (int, float)), "Learning rate must be a number."
     assert isinstance(corpus, str), "Corpus must be a string."
 
-    # Create a list of sentences
-    sentences = []
+    # Create a list of text_sentences
+    text_sentences = []
 
     # Create a list of next characters
     next_chars = []
 
     # Loop through the corpus
-    for i in range(0, len(corpus) - SEQUENCE_LENGTH, STEP_SIZE):
-        if i % 10000 == 0:
-            print(f'Processing {i}/{len(corpus)}')
-        sentences.append(corpus[i: i + SEQUENCE_LENGTH])
-        next_chars.append(corpus[i + SEQUENCE_LENGTH])
+    # For each step, a sentence (of length LEN_SEQ)
+    # and the next character following the sentence are appended to their respective lists.
+    for j in range(0, len(corpus) - LEN_SEQ, STEP_SIZE):
+        if j % 10000 == 0:
+            print(f'Processing {j}/{len(corpus)}')
+        text_sentences.append(corpus[j: LEN_SEQ+j])
+        next_chars.append(corpus[LEN_SEQ+j])
 
-    # Create a list of one-hot encoded sentences
-    train_x = np.zeros((len(sentences), SEQUENCE_LENGTH, len(chars)), dtype=bool)
-    train_y = np.zeros((len(sentences), len(chars)), dtype=bool)
+    # One hot encode characters
+    # train_x's shape corresponds to [number of sentences, length of sentences, number of unique characters in the text]
+    # train_y's shape corresponds to [number of sentences, number of unique characters in the text]
+    train_x = np.zeros(
+        ( len(text_sentences),
+        LEN_SEQ, len(text_chars) ), dtype=bool )
+    train_y = np.zeros(
+        ( len(text_sentences),
+        len(text_chars) ), dtype=bool)
 
-    # Loop through the sentences
-    for i, sentence in enumerate(sentences):
-        if i % 10000 == 0:
-            print(f'Processing {i}/{len(sentences)}')
+    # Loop through the text_sentences
+    # For each sentence, it goes through every character and marks it as 1 in the train_x array.
+    # Then, the next character (in train_y array) for that sentence is also marked as 1.
+    for x, s in enumerate(text_sentences):
+        if x % 10000 == 0:
+            print(f'Processing {x}/{len(text_sentences)}')
         # Loop through the characters in the sentence
-        for j, char in enumerate(sentence):
+        for y, c in enumerate(s):
             # Set the current character to 1
-            train_x[i, j, chars_to_indices[char]] = 1
+            train_x[x, y, chars_to_indices[c]] = 1
         # Set the next character to 1
-        train_y[i, chars_to_indices[next_chars[i]]] = 1
+        train_y[x, chars_to_indices[next_chars[x]]] = 1
 
     # Create the model
     model = Sequential()
 
     # Add an LSTM layer with 128 units
-    model.add(LSTM(128, input_shape=(SEQUENCE_LENGTH, len(chars))))
+    model.add(LSTM(128, input_shape=(LEN_SEQ, len(text_chars))))
 
     # Add a dense layer with the same number of units as characters
-    model.add(Dense(len(chars)))
+    model.add(Dense(len(text_chars)))
 
     # Add a softmax activation layer
     model.add(Activation('softmax'))
 
     # Compile the model
-    model.compile(loss='categorical_crossentropy', optimizer=RMSprop(learning_rate=LEARNING_RATE))
+    model.compile(optimizer=RMSprop(learning_rate=LEARNING_RATE),
+        loss='categorical_crossentropy')
 
     # Train the model
     model.fit(train_x, train_y, epochs=EPOCHS, verbose=1)
